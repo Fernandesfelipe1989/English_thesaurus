92c186466b332e5b313872839fb960b789ecbc69
Felipe <fernandes.felipe1989@gmail.com>
2020-12-20 10:03:32 -0300

    Connect to the database

---
+++ Interactive English Dictionary/main.py
@@ -0,0 +1,45 @@
+import mysql.connector
+from difflib import get_close_matches
+
+con = mysql.connector.connect(
+    user="ardit700_student",
+    password="ardit700_student",
+    host="108.167.140.122",
+    database='ardit700_pm1database'
+)
+
+
+def translate(word):
+    cursor = con.cursor()
+    query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" % word)
+    results = cursor.fetchall()
+    if results:
+        return results
+
+    else:
+        query = cursor.execute(f"SELECT Expression FROM Dictionary WHERE Expression LIKE 'rain%'")
+        results = cursor.fetchall()
+        match_list = []
+        for i in results:
+            match_list.append(i[0])
+        if match_list:
+            word_match = get_close_matches(word, match_list, n=1, cutoff=0.8)
+            yn = input(f"Did you mean any {word_match[0]} instead? Yes or No? ")
+            if yn.lower() == "yes" or yn.lower() == "y":
+                query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" % word_match[0])
+                return cursor.fetchall()
+    return "No way"
+
+
+def main():
+    word = input("Enter a word: ")
+    answer = translate(word)
+    if type(answer) == list:
+        for i in answer:
+            print(i[1])
+    else:
+        print(answer)
+
+
+if __name__ == '__main__':
+    main()
